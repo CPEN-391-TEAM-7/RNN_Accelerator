6ac77a55f9f1c230cf384696dfa76f0f834214ef
ali <aliumar18@gmail.com>
2021-03-06 14:35:03 -0800

    Implement vector w/testbench

--- loader.sv
+++ loader.sv
@@ -32,3 +32,34 @@ end
 assign param_out = matrix[seli][selj];
 
 endmodule
+
+module loader_1d(
+	input  logic        clk,
+	input  logic        reset_n,
+	input  logic        write, 
+	input  logic  [3:0] sel,
+	input  logic [15:0] param_in,
+
+	output logic [15:0] param_out
+	);
+
+logic [15:0] vector [3:0];
+
+integer i;
+always_ff @(posedge clk or negedge reset_n) begin
+	if(~reset_n) begin
+
+		for(i = 0; i <4;i=i+1) begin
+			vector[i] <= 0;
+		end
+
+	end else begin
+
+		if (write) vector[sel] <= param_in;
+
+	end
+end
+
+assign param_out = vector[sel];
+
+endmodule
--- loader_tb.sv
+++ loader_tb.sv
@@ -1,5 +1,5 @@
 
-module loader_tb();
+module matrix_tb();
 
 	logic        clk        ;
 	logic        reset_n    ;
@@ -34,6 +34,46 @@ module loader_tb();
 		seli = 1;
 		@(negedge clk)
 		seli = 0;
+		#10;
+
+		$stop;
+	end
+
+endmodule
+
+module vector_tb();
+
+	logic        clk        ;
+	logic        reset_n    ;
+	logic        write      ;
+	logic  [3:0] sel        ;
+	logic [15:0] param_in   ;
+
+	logic [15:0] param_out  ;
+
+	loader_1d dut(.*);
+
+	always begin
+		#1 clk = 0;
+		#1 clk = 1;
+	end
+
+	initial begin 
+		reset_n  = 0;
+		write    = 0;
+		sel      = 0;
+		param_in = 0; 
+		#5;
+		reset_n  = 1;
+		param_in = 16'hBEEF;
+		write    = 1;
+		@(posedge clk)
+		@(posedge clk)
+		write    = 0;
+		#4;
+		sel = 1;
+		@(negedge clk)
+		sel = 0;
 		#10;
 
 		$stop;
