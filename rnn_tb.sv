cea734e01960f8538e38e09207f6de4f3ac8511c
ali <aliumar18@gmail.com>
2021-03-08 15:32:09 -0800

    Improve test bench and add actual test values

--- rnn_tb.sv
+++ rnn_tb.sv
@@ -9,11 +9,28 @@ module rnn_tb();
 	logic [31:0] data_in;
 	logic [31:0] data_out;
 
+	integer r0;
+	integer c0;
+	integer s0;
+
 	integer r1;
 	integer c1;
 	integer s1;
 
 	rnn dut(.*);
+
+	logic [15:0] embed_v [1:0] = {-3,2};
+
+	logic [15:0] rnn0_m[0:1][0:3];
+	assign rnn0_m[0] = { 2,-10,-10, 3};
+	assign rnn0_m[1] = { 6,  9, 12, 1};
+
+	logic [15:0] rnn1_m[0:3][0:3];
+	assign rnn1_m[0] = { -2, -3, -5,  -3};
+	assign rnn1_m[1] = { -1, 10, -2,  -6};
+	assign rnn1_m[2] = {  4, 11,  3, -12};
+	assign rnn1_m[3] = {-11, -4,  3,  -1};
+
 
 	always begin
 		#1 clk = 0;
@@ -39,10 +56,10 @@ module rnn_tb();
 		@(negedge clk)
 		write   <= 1;
 		addr    <= 1;
-		data_in <= {16'b0,16'd2};
+		data_in <= {16'b0,embed_v[0]};
 		@(negedge clk)
 		assert (dut.input_char.vector[0] === 16'd2);
-		data_in <= {16'b1,16'(-3)};
+		data_in <= {16'b1,embed_v[1]};
 
 		@(negedge clk)
 		assert (dut.input_char.vector[1] === 16'(-3));
@@ -61,37 +78,14 @@ module rnn_tb();
 		write   <= 1;
 		addr    <= 2;
 
-		data_in <= {8'd0,8'd0,16'd2};
-		@(negedge clk)
-		assert (dut.rnn_0.matrix[0][0] === 16'd2);
+		for (r0=0; r0< 2; r0=r0+1) begin
+			for (c0=0; c0< 4; c0=c0+1) begin
+				data_in <= {r0[7:0], c0[7:0], rnn0_m[r0][c0]};
+				@(negedge clk)
+				assert(dut.rnn_0.matrix[r0][c0] === rnn0_m[r0][c0]);
+			end
+		end
 
-		data_in <= {8'd0,8'd1,16'(-10)};
-		@(negedge clk)
-		assert (dut.rnn_0.matrix[0][1] === 16'(-10));
-
-		data_in <= {8'd0,8'd2,16'(-10)};
-		@(negedge clk)
-		assert (dut.rnn_0.matrix[0][2] === 16'(-10));
-
-		data_in <= {8'd0,8'd3,16'd3};
-		@(negedge clk)
-		assert (dut.rnn_0.matrix[0][3] === 16'd3);
-
-		data_in <= {8'd1,8'd0,16'd6};
-		@(negedge clk)
-		assert (dut.rnn_0.matrix[1][0] === 16'd6);
-
-		data_in <= {8'd1,8'd1,16'd9};
-		@(negedge clk)
-		assert (dut.rnn_0.matrix[1][1] === 16'd9);
-
-		data_in <= {8'd1,8'd2,16'd12};
-		@(negedge clk)
-		assert (dut.rnn_0.matrix[1][2] === 16'd12);
-
-		data_in <= {8'd1,8'd3,16'd1};
-		@(negedge clk)
-		assert (dut.rnn_0.matrix[1][3] === 16'd1);
 		write <= 0;
 		#10;
 		// ==========================================================
@@ -104,10 +98,9 @@ module rnn_tb();
 
 		for (r1=0; r1< 4; r1=r1+1) begin
 			for (c1=0; c1< 4; c1=c1+1) begin
-				s1 = r1 + c1;
-				data_in <= {r1[7:0], c1[7:0], s1[15:0]};
+				data_in <= {r1[7:0], c1[7:0], rnn1_m[r1][c1]};
 				@(negedge clk)
-				assert(dut.rnn_1.matrix[r1][c1] == s1[15:0]);
+				assert(dut.rnn_1.matrix[r1][c1] === rnn1_m[r1][c1]);
 			end
 		end
 
